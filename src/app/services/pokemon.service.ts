c33d7257ad668f4d97df754231eeba1c50a2b0bc
Rodrigo Valença <rodrigovalenca@MacBook-Pro-de-Rodrigo.local>
2023-02-17 15:19:10 -0300

    Update the name pokemon service

---
+++ src/app/services/pokemon.service.ts
@@ -0,0 +1,14 @@
+/*
+  Esse trata-se de um componente Injetável, em outras palavras,
+  esse Service pode ser injetado em um outro componente que utilizará
+  de suas funcionalidades.
+*/
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root' //Significa que todos os demais componentes possuem acesso a esse service
+})
+export class PokemonService {
+
+  constructor() { }
+}
