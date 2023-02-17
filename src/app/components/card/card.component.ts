26de68b0514208350b203e43a0a8c5aed4ef7d82
Rodrigo Valença <rodrigovalenca@MacBook-Pro-de-Rodrigo.local>
2023-02-17 08:49:18 -0300

    Uncoupling the Component

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent {
+export class CardComponent implements OnInit{
 
+  name: string = "BULBASSAUR"
+  atributesTypes: string[] = ['FIRE', 'ROCK']
+
+  constructor() {}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 }
