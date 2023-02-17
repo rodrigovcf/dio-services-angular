a1d7506af36de2dbeb582cc165400ab739615088
Rodrigo Valença <rodrigovalenca@MacBook-Pro-de-Rodrigo.local>
2023-02-17 16:00:47 -0300

    Service comunication

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -4,15 +4,21 @@
   de suas funcionalidades.
 */
 import { Injectable } from '@angular/core';
+import {} from 'src/environments/environment';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root' //Significa que todos os demais componentes possuem acesso a esse service
 })
 export class PokemonService {
+  private baseURL:string = ""
 
-  constructor() { }
+  constructor() {
+    //Inicializando a baseURL por meio da pokeApi contida no environment
+    this.baseURL = environment.pokeApi
+  }
 
   getPokemon(pokemonName:string){
-    console.log(pokemonName)
+    console.log(this.baseURL)
   }
 }
